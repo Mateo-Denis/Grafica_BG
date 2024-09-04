964b24b2d9f94e3e557ff06eae5d0ab640abd872
mateo <mateojdenis@gmail.com>
2024-09-03 23:26:22 -0300

    Settings view setup

---
+++ src/main/java/views/settings/SettingsView.java
@@ -0,0 +1,20 @@
+package views.settings;
+
+import javax.swing.*;
+
+public class SettingsView {
+	private JPanel containerPanel;
+	private JPanel generalValuesPanel;
+	private JTextField plankLoweringTextField;
+	private JPanel clothValuesPanel;
+	private JTable clothValuesTable;
+	private JTextField capTextField;
+	private JTextField cupTextField;
+	private JPanel clothesValuesPanel;
+	private JTable clothesValuesTable;
+	private JButton updateDataButton;
+	private JTextField inkTextField;
+	private JLabel inkLabel;
+
+
+}
