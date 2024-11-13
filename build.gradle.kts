c0927a9d58009e65a069be56540cfbb34a05d728
Gonzalo Girotti <gongirotti2001@gmail.com>
2024-11-13 10:51:56 -0300

    Updated build.gradle file
     I added some configuration blocks to the build.gradle file to be able to build the .exe of the program.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,23 @@
 plugins {
     id("java")
+    id("com.github.johnrengelman.shadow") version("8.1.1")
+    id("application")
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
+application {
+    mainClass.set("main.Main")
+}
+
 repositories {
     mavenCentral()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
@@ -17,20 +28,19 @@ dependencies {
     implementation("org.slf4j:slf4j-simple:1.7.36")
     testImplementation(kotlin("test"))
     implementation("org.reflections:reflections:0.10.2")
-    compileOnly ("org.projectlombok:lombok:1.18.34")
-    annotationProcessor ("org.projectlombok:lombok:1.18.34")
-    testCompileOnly ("org.projectlombok:lombok:1.18.34")
-    testAnnotationProcessor ("org.projectlombok:lombok:1.18.34")
+    compileOnly("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
+    testCompileOnly("org.projectlombok:lombok:1.18.34")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.34")
     implementation("com.itextpdf:kernel:8.0.2")
     implementation("com.itextpdf:layout:8.0.5")
-    implementation ("com.jgoodies:jgoodies-forms:2.13.0")
-    implementation ("org.swinglabs.swingx:swingx-all:1.6.5-1")
-    implementation ("org.javatuples:javatuples:1.2")
-    implementation ("com.google.guava:guava:32.1.2-jre")
-    implementation ("com.formdev:flatlaf:3.5.1")
-    implementation ("com.fasterxml.jackson.core:jackson-databind:2.13.3")
-    implementation ("com.google.code.gson:gson:2.8.9")
-
+    implementation("com.jgoodies:jgoodies-forms:1.6.0")
+    implementation("org.swinglabs.swingx:swingx-all:1.6.5-1")
+    implementation("org.javatuples:javatuples:1.2")
+    implementation("com.google.guava:guava:32.1.2-jre")
+    implementation("com.formdev:flatlaf:3.5.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
+    implementation("com.google.code.gson:gson:2.8.9")
 }
 
 tasks.test {
